d6bc4be5518badc244d72a98c566a6170a177116
Nmstr <publicNamester@gmail.com>
2025-08-04 18:42:17 +0200

    created common function for sending requests to the server

--- SubDrome/api_handler.py
+++ SubDrome/api_handler.py
@@ -14,6 +14,31 @@ class ApiHandler(QObject):
         self.config_handler = config_handler
         self.thread_manager = QThreadPool()
 
+    def _send_request(self, endpoint: str, extra_params: dict = None) -> dict:
+        """
+        Helper method to send a request to the Subsonic server.
+        :param endpoint: The API endpoint to call.
+        :param extra_params: (optional) Additional parameters for the request.
+        :return: The JSON response from the server.
+        """
+        params = {
+            "u": self.config_handler.username,
+            "t": self.config_handler.token,
+            "s": self.config_handler.salt,
+            "c": "SubDromeClient",
+            "v": "1.0",
+            "f": "json"
+        }
+        if extra_params:
+            params.update(extra_params)
+        try:
+            response = requests.get(f"{self.config_handler.server_address}/rest/{endpoint}", params=params)
+            if response.status_code == 200:
+                return response.json().get("subsonic-response", {})
+        except requests.RequestException:
+            pass
+        return {}
+
     def get_cover_art(self, cover_id: str, album_id: str = None) -> str:
         """
         Fetch cover art from the server.
@@ -30,6 +55,7 @@ class ApiHandler(QObject):
                 self.coverReady.emit(album_id, cover_file_path)
             return cover_file_path
 
+        # This doesn't use _send_request because the endpoint does not return JSON data
         params = {
             "u": self.config_handler.username,
             "t": self.config_handler.token,
@@ -61,33 +87,24 @@ class ApiHandler(QObject):
         Fetch random albums from the server.
         :return: A list of random albums or an empty list if the request fails.
         """
-        params = {
-            "u": self.config_handler.username,
-            "t": self.config_handler.token,
-            "s": self.config_handler.salt,
-            "c": "SubDromeClient",
-            "v": "1.0",
-            "f": "json",
+        extra_params = {
             "type": album_type,
             "size": 20
         }
-        try:
-            response = requests.get(f"{self.config_handler.server_address}/rest/getAlbumList2", params=params)
-            if response.status_code == 200 and response.json().get("subsonic-response", {}).get("status") == "ok":
-                albums = []
-                for album in response.json().get("subsonic-response", {}).get("albumList2", {}).get("album", []):
-                    cover_id = album.get("coverArt", "")
-                    album_id = album.get("id", "")
-                    self.thread_manager.start(lambda cid=cover_id, aid=album_id: self.get_cover_art(cid, aid))
-                    albums.append([
-                        album_id,
-                        album.get("name"),
-                        album.get("artist"),
-                        f"{os.getcwd()}/assets/icons/material/album.svg"
-                    ])
-                self.albumsUpdated.emit(albums)
-        except requests.RequestException:
-            pass
+        response = self._send_request("getAlbumList2", extra_params)
+        if response.get("status") == "ok":
+            albums = []
+            for album in response.get("albumList2", {}).get("album", []):
+                cover_id = album.get("coverArt", "")
+                album_id = album.get("id", "")
+                self.thread_manager.start(lambda cid=cover_id, aid=album_id: self.get_cover_art(cid, aid))
+                albums.append([
+                    album_id,
+                    album.get("name"),
+                    album.get("artist"),
+                    f"{os.getcwd()}/assets/icons/material/album.svg"
+                ])
+            self.albumsUpdated.emit(albums)
 
     @Slot(str)
     def get_album_details(self, album_id: str) -> dict:
@@ -96,40 +113,31 @@ class ApiHandler(QObject):
         :param album_id: The ID of the album to fetch details for.
         :return: A dictionary containing album details or an empty dictionary if the request fails.
         """
-        params = {
-            "u": self.config_handler.username,
-            "t": self.config_handler.token,
-            "s": self.config_handler.salt,
-            "c": "SubDromeClient",
-            "v": "1.0",
-            "f": "json",
+        extra_params = {
             "id": album_id
         }
-        try:
-            response = requests.get(f"{self.config_handler.server_address}/rest/getAlbum", params=params)
-            if response.status_code == 200 and response.json().get("subsonic-response", {}).get("status") == "ok":
-                album_details = response.json().get("subsonic-response", {}).get("album", {})
+        response = self._send_request("getAlbum", extra_params)
+        if response.get("status") == "ok":
+           album_details = response.get("album", {})
 
-                song_list = []
-                for song in album_details.get("song", []):
-                    song_list.append([
-                        song.get("id", ""),
-                        song.get("title", ""),
-                        song.get("artist", ""),
-                        song.get("duration", 0),
-                    ])
+           song_list = []
+           for song in album_details.get("song", []):
+               song_list.append([
+                   song.get("id", ""),
+                   song.get("title", ""),
+                   song.get("artist", ""),
+                   song.get("duration", 0),
+               ])
 
-                cover_art_path = self.get_cover_art(album_details.get("coverArt", ""))
-                self.albumDetailsReceived.emit(
-                    album_details.get("id", ""),
-                    album_details.get("name"),
-                    album_details.get("artist"),
-                    cover_art_path,
-                    song_list
-                )
-                return response.json().get("subsonic-response", {}).get("album", {})
-        except requests.RequestException:
-            pass
+           cover_art_path = self.get_cover_art(album_details.get("coverArt", ""))
+           self.albumDetailsReceived.emit(
+               album_details.get("id", ""),
+               album_details.get("name"),
+               album_details.get("artist"),
+               cover_art_path,
+               song_list
+           )
+           return album_details
         return {}
 
     def get_song_details(self, song_id: str) -> dict:
@@ -138,21 +146,12 @@ class ApiHandler(QObject):
         :param song_id: The ID of the song to fetch details for.
         :return: A dictionary containing song details or an empty dictionary if the request fails.
         """
-        params = {
-            "u": self.config_handler.username,
-            "t": self.config_handler.token,
-            "s": self.config_handler.salt,
-            "c": "SubDromeClient",
-            "v": "1.0",
-            "f": "json",
+        extra_params = {
             "id": song_id
         }
-        try:
-            response = requests.get(f"{self.config_handler.server_address}/rest/getSong", params=params)
-            if response.status_code == 200 and response.json().get("subsonic-response", {}).get("status") == "ok":
-                return response.json().get("subsonic-response", {}).get("song", {})
-        except requests.RequestException:
-            pass
+        response = self._send_request("getSong", extra_params)
+        if response.get("status") == "ok":
+            return response.get("song", {})
         return {}
 
     def download_song(self, song_id: str) -> str:
@@ -161,6 +160,7 @@ class ApiHandler(QObject):
         :param song_id: The ID of the song to download.
         :return: The path to the downloaded song file.
         """
+        # This doesn't use _send_request because the endpoint does not return JSON data
         params = {
             "u": self.config_handler.username,
             "t": self.config_handler.token,
@@ -190,60 +190,40 @@ class ApiHandler(QObject):
         Search for albums
         :param query: The search query.
         """
-        params = {
-            "u": self.config_handler.username,
-            "t": self.config_handler.token,
-            "s": self.config_handler.salt,
-            "c": "SubDromeClient",
-            "v": "1.0",
-            "f": "json",
+        extra_params = {
             "query": query
         }
-        try:
-            response = requests.get(f"{self.config_handler.server_address}/rest/search2", params=params)
-            if response.status_code == 200 and response.json().get("subsonic-response", {}).get("status") == "ok":
-                albums = []
-                for album in response.json().get("subsonic-response", {}).get("searchResult2", {}).get("album", []):
-                    cover_id = album.get("coverArt", "")
-                    album_id = album.get("id", "")
-                    self.thread_manager.start(lambda cid=cover_id, aid=album_id: self.get_cover_art(cid, aid))
-                    albums.append([
-                        album_id,
-                        album.get("name"),
-                        album.get("artist"),
-                        f"{os.getcwd()}/assets/icons/material/album.svg"
-                    ])
-                self.albumsUpdated.emit(albums)
-        except requests.RequestException:
-            pass
+        response = self._send_request("search2", extra_params)
+        if response.get("status") == "ok":
+            albums = []
+            for album in response.get("searchResult2", {}).get("album", []):
+                cover_id = album.get("coverArt", "")
+                album_id = album.get("id", "")
+                self.thread_manager.start(lambda cid=cover_id, aid=album_id: self.get_cover_art(cid, aid))
+                albums.append([
+                    album_id,
+                    album.get("name"),
+                    album.get("artist"),
+                    f"{os.getcwd()}/assets/icons/material/album.svg"
+                ])
+            self.albumsUpdated.emit(albums)
 
     @Slot()
     def update_playlist_list(self) -> None:
         """
         Fetch the list of playlists from the server.
         """
-        params = {
-            "u": self.config_handler.username,
-            "t": self.config_handler.token,
-            "s": self.config_handler.salt,
-            "c": "SubDromeClient",
-            "v": "1.0",
-            "f": "json"
-        }
-        try:
-            response = requests.get(f"{self.config_handler.server_address}/rest/getPlaylists", params=params)
-            if response.status_code == 200 and response.json().get("subsonic-response", {}).get("status") == "ok":
-                playlists = []
-                for playlist in response.json().get("subsonic-response", {}).get("playlists", {}).get("playlist", []):
-                    cover_art_path = self.get_cover_art(playlist.get("coverArt", ""))
-                    playlists.append([
-                        playlist.get("id", ""),
-                        playlist.get("name", ""),
-                        cover_art_path
-                    ])
-                self.playlistListChanged.emit(playlists)
-        except requests.RequestException:
-            pass
+        response = self._send_request("getPlaylists")
+        if response.get("status") == "ok":
+            playlists = []
+            for playlist in response.get("playlists", {}).get("playlist", []):
+                cover_art_path = self.get_cover_art(playlist.get("coverArt", ""))
+                playlists.append([
+                    playlist.get("id", ""),
+                    playlist.get("name", ""),
+                    cover_art_path
+                ])
+            self.playlistListChanged.emit(playlists)
 
     @Slot(str)
     def get_playlist_details(self, playlist_id: str) -> dict:
@@ -251,36 +231,27 @@ class ApiHandler(QObject):
         Fetch details of a specific playlist from the server.
         :param playlist_id: The ID of the playlist to fetch details for.
         """
-        params = {
-            "u": self.config_handler.username,
-            "t": self.config_handler.token,
-            "s": self.config_handler.salt,
-            "c": "SubDromeClient",
-            "v": "1.0",
-            "f": "json",
+        extra_params = {
             "id": playlist_id
         }
-        try:
-            response = requests.get(f"{self.config_handler.server_address}/rest/getPlaylist", params=params)
-            if response.status_code == 200 and response.json().get("subsonic-response", {}).get("status") == "ok":
-                playlist_details = response.json().get("subsonic-response", {}).get("playlist", {})
-                song_list = []
-                for song in playlist_details.get("entry", []):
-                    song_list.append([
-                        song.get("id", ""),
-                        song.get("title", ""),
-                        song.get("artist", ""),
-                        song.get("duration", 0),
-                    ])
-                cover_art_path = self.get_cover_art(playlist_details.get("coverArt", ""))
-                self.playlistDetailsReceived.emit(
-                    playlist_details.get("id", ""),
-                    playlist_details.get("name"),
-                    playlist_details.get("owner", ""),
-                    cover_art_path,
-                    song_list
-                )
-                return response.json().get("subsonic-response", {}).get("playlist", {})
-        except requests.RequestException:
-            pass
+        response = self._send_request("getPlaylist", extra_params)
+        if response.get("status") == "ok":
+            playlist_details = response.get("playlist", {})
+            song_list = []
+            for song in playlist_details.get("entry", []):
+                song_list.append([
+                    song.get("id", ""),
+                    song.get("title", ""),
+                    song.get("artist", ""),
+                    song.get("duration", 0),
+                ])
+            cover_art_path = self.get_cover_art(playlist_details.get("coverArt", ""))
+            self.playlistDetailsReceived.emit(
+                playlist_details.get("id", ""),
+                playlist_details.get("name"),
+                playlist_details.get("owner", ""),
+                cover_art_path,
+                song_list
+            )
+            return playlist_details
         return {}
