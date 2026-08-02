eedce572b03783657593b3877eb6ffa9e62e53ec
Nmstr <publicNamester@gmail.com>
2026-08-03 01:07:15 +0200

    implemented basic config

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use crate::config;
+use crate::config::Config;
 use slint::ComponentHandle;
 use std::error::Error;
 
@@ -5,18 +7,32 @@ slint::include_modules!();
 
 pub struct App {
     window: MainWindow,
+    config: Config,
 }
 
 impl App {
     pub fn new() -> Result<Self, Box<dyn Error>> {
         let window = MainWindow::new()?;
+        let config = Config::load()?;
         slint::set_xdg_app_id("SubDrome")?;
 
-        let app = Self { window };
+        let app = Self { window, config };
         Ok(app)
     }
 
-    pub fn run(&self) -> Result<(), slint::PlatformError> {
+    pub fn run(&mut self) -> Result<(), slint::PlatformError> {
+        self.config.active_username = Some(String::from("test"));
+        self.config.active_salt = Some(String::from("test-salt"));
+        self.config.save().expect("Failed to save config");
+
+        let digest = md5::compute(
+            self.config.active_username.clone().unwrap_or_default()
+                + &self.config.active_salt.clone().unwrap_or_default(),
+        );
+        config::save_credentials("test", &format!("{:x}", digest))
+            .expect("Failed to save credentials");
+        println!("{:?}", config::load_credentials("test"));
+
         self.window.run().map_err(Into::into)
     }
 }
---
+++ src/config.rs
@@ -0,0 +1,47 @@
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+const KEYRING_SERVICE: &str = "SubDrome";
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Config {
+    pub server_url: Option<String>,
+    pub active_username: Option<String>,
+    pub active_salt: Option<String>,
+}
+
+impl Config {
+    pub fn load() -> Result<Self, std::io::Error> {
+        let path = Self::path()?;
+        if !path.exists() {
+            return Ok(Self::default());
+        }
+        Ok(serde_json::from_str(&std::fs::read_to_string(path)?)?)
+    }
+
+    pub fn save(&self) -> Result<(), std::io::Error> {
+        let path = Self::path()?;
+        if let Some(parent) = path.parent() {
+            std::fs::create_dir_all(parent)?;
+        }
+        std::fs::write(path, serde_json::to_string_pretty(self)?)?;
+        Ok(())
+    }
+
+    pub fn path() -> Result<PathBuf, std::io::Error> {
+        let dirs = directories::ProjectDirs::from("org", "LightDrive", "SubDrome")
+            .ok_or_else(|| std::io::ErrorKind::NotFound)?;
+        Ok(dirs.config_dir().join("config.json"))
+    }
+}
+
+pub fn save_credentials(username: &str, token: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let entry = keyring::Entry::new(&KEYRING_SERVICE, username);
+    entry?.set_password(token)?;
+    Ok(())
+}
+
+pub fn load_credentials(username: &str) -> Result<String, keyring::Error> {
+    let entry = keyring::Entry::new(&KEYRING_SERVICE, username);
+    entry?.get_password()
+}
